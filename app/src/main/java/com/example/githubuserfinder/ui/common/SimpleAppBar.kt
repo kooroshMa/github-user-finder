87edcf52659ee31dc3b1a25fe474edab0f1dc901
koorosh <koorosh_mashhadi@yahoo.com>
2024-06-20 01:15:34 -0400

    Corrected SimpleAppBar.

--- app/src/main/java/com/example/githubuserfinder/ui/common/SimpleAppBar.kt
+++ app/src/main/java/com/example/githubuserfinder/ui/common/SimpleAppBar.kt
@@ -1,5 +1,8 @@
 package com.example.githubuserfinder.ui.common
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -17,6 +20,33 @@ import com.example.githubuserfinder.R
 import com.example.githubuserfinder.ui.theme.ThemePreview
 import com.example.githubuserfinder.ui.theme.space
 
+
+/**
+ * SimpleTopAppBar for our first page. We do not need navigation icon in first page
+ */
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SimpleTopAppBar(title: String) {
+    TopAppBar(
+        title = {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Start,
+            ) {
+                Text(
+                    text = title,
+                    style = MaterialTheme.typography.headlineSmall,
+                    color = MaterialTheme.colorScheme.onBackground,
+                )
+            }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+            navigationIconContentColor = MaterialTheme.colorScheme.onBackground
+        )
+    )
+}
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SimpleTopAppBar(
@@ -24,50 +54,34 @@ fun SimpleTopAppBar(
     onBackClick: () -> Unit
 ) {
     TopAppBar(
+        title = {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Start,
+            ) {
+                Text(
+                    text = title,
+                    style = MaterialTheme.typography.headlineSmall,
+                    color = MaterialTheme.colorScheme.onBackground,
+                )
+            }
+        },
         colors = TopAppBarDefaults.topAppBarColors(
-            containerColor = MaterialTheme.colorScheme.background,
+            containerColor = MaterialTheme.colorScheme.primary,
             navigationIconContentColor = MaterialTheme.colorScheme.onBackground
         ),
-        title = {
-            Text(
-                text = title,
-                style = MaterialTheme.typography.headlineSmall,
-                color = MaterialTheme.colorScheme.onBackground
-            )
-        },
         navigationIcon = {
             IconButton(onClick = onBackClick) {
                 Icon(
-                    modifier = Modifier.size(MaterialTheme.space.xMedium).testTag("back"),
+                    modifier = Modifier
+                        .size(MaterialTheme.space.xMedium)
+                        .testTag("back"),
                     painter = painterResource(id = R.drawable.ic_back),
                     contentDescription = "back",
                     tint = MaterialTheme.colorScheme.onBackground
                 )
             }
         }
-    )
-}
-
-/**
- * SimpleTopAppBar for our first page. We do not need navigation icon in first page
- */
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun SimpleTopAppBar(
-    title: String,
-) {
-    CenterAlignedTopAppBar(
-        colors = TopAppBarDefaults.topAppBarColors(
-            containerColor = MaterialTheme.colorScheme.background,
-            navigationIconContentColor = MaterialTheme.colorScheme.onBackground
-        ),
-        title = {
-            Text(
-                text = title,
-                style = MaterialTheme.typography.headlineSmall,
-                color = MaterialTheme.colorScheme.onBackground
-            )
-        },
     )
 }
 
