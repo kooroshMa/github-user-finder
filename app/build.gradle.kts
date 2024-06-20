395ef52214dd2fe0c17f20b73373ade4dd2da60a
koorosh <koorosh_mashhadi@yahoo.com>
2024-06-20 19:33:30 -0400

    Added mockito-kotlin dependency.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,6 +74,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.window.size.clazz)
     testImplementation(libs.junit)
+    testImplementation(libs.mockito.kotlin)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
